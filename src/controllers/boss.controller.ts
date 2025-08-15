f6d42adebc045b260c6db4f7ac0fcaae4136bd20
Alex Appleget <alexappleget2014@gmail.com>
2025-08-15 14:09:49 -0500

    added in functionality to toggle between boss killed and alive

--- src/controllers/boss.controller.ts
+++ src/controllers/boss.controller.ts
@@ -9,17 +9,44 @@ export const getWorldBosses = async (
   try {
     const { id } = request.params;
 
-    const worldBosses = await prisma.boss.findMany({
+    const worldBossesRaw = await prisma.boss.findMany({
       include: {
         worldProgress: {
           where: {
             worldId: id,
           },
+          take: 1,
         },
       },
     });
 
+    const worldBosses = worldBossesRaw.map((boss) => ({
+      ...boss,
+      worldProgress: boss.worldProgress[0],
+    }));
+
     return response.status(200).json({ bosses: worldBosses });
+  } catch (error) {
+    return response.status(500).json({ error });
+  }
+};
+
+export const setBossKilledState = async (
+  request: IAuthenticatedRequest,
+  response: Response
+) => {
+  try {
+    const { id } = request.params;
+    const { killed } = request.body;
+
+    await prisma.worldBossProgress.update({
+      where: { id },
+      data: {
+        killed,
+      },
+    });
+
+    return response.status(200).json({ success: true });
   } catch (error) {
     return response.status(500).json({ error });
   }
--- src/routes/boss.routes.ts
+++ src/routes/boss.routes.ts
@@ -1,9 +1,20 @@
 import express from "express";
 import { requireAuth } from "../middleware/requireAuth";
-import { getWorldBosses } from "../controllers/boss.controller";
+import {
+  getWorldBosses,
+  setBossKilledState,
+} from "../controllers/boss.controller";
+import { requireRole } from "../middleware/requireRole";
 
 const router = express.Router();
 
 router.get("/:id", requireAuth, getWorldBosses);
+
+router.patch(
+  "/:id",
+  requireAuth,
+  requireRole(["OWNER", "ADMIN", "SUB_ADMIN"]),
+  setBossKilledState
+);
 
 export default router;
