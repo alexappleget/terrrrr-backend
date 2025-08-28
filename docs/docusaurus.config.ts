9f36ca2d433feaa920d76afbe23fd30de47c28e0
Alex Appleget <alexappleget2014@gmail.com>
2025-08-27 20:15:49 -0500

    edited copyright

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -99,7 +99,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Alex Appleget`,
     },
     prism: {
       theme: prismThemes.github,
