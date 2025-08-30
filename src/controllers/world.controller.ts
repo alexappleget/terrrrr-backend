24ffc473de85f3bfae12bf9a9e348b59ae6b55da
Alex Appleget <alexappleget2014@gmail.com>
2025-08-30 09:35:50 -0500

    handled comments

--- src/controllers/world.controller.ts
+++ src/controllers/world.controller.ts
@@ -101,10 +101,6 @@ export const getUserWorlds = async (
   try {
     const { id } = request.user!;
 
-    if (!id) {
-      return response.status(400).json({ message: "UserId not provided" });
-    }
-
     const userWorldMemberships = await prisma.worldMembership.findMany({
       where: {
         userId: id,
