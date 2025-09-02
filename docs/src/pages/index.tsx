6d67ca305f7824e1ee7219833c5fd6e851299f9d
Alex Appleget <alexappleget2014@gmail.com>
2025-09-02 14:22:39 -0500

    updated layout description

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home(): ReactNode {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description="All backend documentation for the Terrrrr project."
     >
       <HomepageHeader />
       <main>
